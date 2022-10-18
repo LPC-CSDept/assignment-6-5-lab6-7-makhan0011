109a0da6bfbb19752cc14bfa81c0c900c6add8b0
makhan0011 <112144798+makhan0011@users.noreply.github.com>
2022-10-18 14:46:08 -0700

    Update A65.hpp

--- A65.hpp
+++ A65.hpp
@@ -9,3 +9,23 @@ using namespace std;
 //              This function will use swaptwo().
 
 // swaptwo() ; to swap two values (a, b) to (b, a)
+
+// initiate function swapTwo  
+void swapTwo(int &num1, int &num2);
+
+// swap function that swaps num1, num2, and num3
+void swap(int &num1, int &num2, int &num3)
+{
+    swaptwo(num1, num2);
+    swaptwo(num1, num3);
+}
+
+// swapTwo function that swaps two int
+void swapTwo(int &num1, int &num2)
+{
+    int a, b; 
+    a = num1;
+    b = num2; 
+    num1 = b; 
+    num2 = a; 
+}
